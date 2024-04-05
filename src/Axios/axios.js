8b07d6574013552bbf2a7fc9d17aadadfefba382
naivecoder02 <113258660+naivecoder02@users.noreply.github.com>
2024-04-05 19:23:27 +0530

    Update axios.js

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"https://taskmanager-api-gf3o.onrender.com"
+    baseURL:"https://taskmanager-api-uces.onrender.com"
 })
 export default instance
